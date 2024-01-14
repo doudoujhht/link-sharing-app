c6fa85237fc0e209d230432cafbc141328b38e12
doudoujhht <poundensigue@gmail.com>
2024-01-14 14:56:09 -0500

    the navbar is done

---
+++ src/lib/components/NavBar.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    import logo from '$lib/assets/images/logo-devlinks-large.svg';
+</script>
+
+<nav class="">
+    <img src="{logo}" alt="">
+    <div>
+        <button class="text-purple active">Links</button>
+        <button class="text-purple">Profile Details</button>
+    </div>
+    <button class="btn-secondary">Preview</button>
+</nav>
+
+
+
+
+
+
+
+
+<style>
+    .active{
+        background-color: #EFEBFF;
+    }
+
+    nav{
+        display: flex;
+        padding: 16px 16px 16px 24px;
+        justify-content: space-between;  
+        border-radius: 0.5rem;
+        background-color: white;  
+        margin-bottom: 24px;  
+    }
+</style>
---
+++ src/routes/links/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import NavBar from '$lib/components/NavBar.svelte';
+    import frame from '$lib/assets/images/illustration-phone-mockup.svg';
+</script>
+
+<NavBar />
+
+
+
+
+
+
+
+<style>
+    :global(body){
+        background-color: #FaFaFa;
+        padding: 24px;
+        min-height: 100vh;
+    }
+
+</style>
